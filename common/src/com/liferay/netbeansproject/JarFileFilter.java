62a7f2fd317574a33da94773ae9c69e572313a15
tomwang2011 <tong.wang@liferay.com>
2015-10-08 09:35:34 -0700

    Gradle Changes

---
+++ common/src/com/liferay/netbeansproject/JarFileFilter.java
@@ -0,0 +1,14 @@
+package com.liferay.netbeansproject;
+
+import java.io.File;
+import java.io.FileFilter;
+public class JarFileFilter implements FileFilter {
+
+	@Override
+	public boolean accept(File file) {
+		String fileName = file.getName();
+
+		return fileName.endsWith(".jar");
+	}
+
+}
